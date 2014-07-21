021f8fd329de07bf123acec05b75b6a2ff4435a3
David Bradford <dbradford@omniti.com>
2014-07-21 17:59:15 -0400

    added gitignore and started an objdemo file

--- ObjDemo.java
+++ ObjDemo.java
@@ -1,27 +1,33 @@
 class Bicycle {
 
-    // the Bicycle class has
-    // three fields
+    // the Bicycle class has three fields
     public int cadence;
     public int gear;
     public int speed;
 
-    // the Bicycle class has
-    // one constructor
+    // the Bicycle class has one constructor
     public Bicycle(int startCadence, int startSpeed, int startGear) {
         gear = startGear;
         cadence = startCadence;
         speed = startSpeed;
+        System.out.println("j3 constructor");
+        reportInfo();
     }
 
-    // the Bicycle class has
-    // four methods
+    public void reportInfo() {
+        System.out.println("j2 cadence: " + cadence + " speed: " + speed + " gear: " + gear);
+    }
+
+    // the Bicycle class has four methods
     public void setCadence(int newValue) {
         cadence = newValue;
     }
 
     public void setGear(int newValue) {
+        int wasgear = gear;
         gear = newValue;
+        System.out.println("j1 setGear(" + newValue + ")");
+        reportInfo();
     }
 
     public void applyBrake(int decrement) {
@@ -35,23 +41,19 @@ class Bicycle {
 }
 
 // A class declaration for a MountainBike class that is a subclass of Bicycle might look like this:
-
 class MountainBike extends Bicycle {
 
-    // the MountainBike subclass has
-    // one field
+    // the MountainBike subclass has one field
     public int seatHeight;
 
-    // the MountainBike subclass has
-    // one constructor
+    // the MountainBike subclass has one constructor
     public MountainBike(int startHeight, int startCadence,
                         int startSpeed, int startGear) {
         super(startCadence, startSpeed, startGear);
         seatHeight = startHeight;
     }
 
-    // the MountainBike subclass has
-    // one method
+    // the MountainBike subclass has one method
     public void setHeight(int newValue) {
         seatHeight = newValue;
     }
@@ -59,24 +61,10 @@ class MountainBike extends Bicycle {
 }
 
 
-public class SwitchDemo {
+public class ObjDemo {
     public static void main(String[] args) {
-
-        int nummy = 3;
-        // nummy = Integer.parseInt(args[0]);
-        String nummyString;
         Bicycle frank = new Bicycle(5,7,10);
-        switch (nummy) {
-            case 1:  nummyString = "One";
-                     break;
-            case 2:  nummyString = "Two";
-                     break;
-            case 3:  nummyString = "Three";
-                     break;
-            default: nummyString = "Invalid nummy";
-                     break;
-        }
-        System.out.println(nummyString);
+        frank.setGear(12);
     }
 }
 
