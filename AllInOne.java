085f7a14bbdd3d5c165e4799597fb84044fa2372
David Bradford <dbradford@omniti.com>
2014-07-28 10:37:43 -0400

    lots more stuff

--- AllInOne.java
+++ AllInOne.java
@@ -1,49 +1,78 @@
 import java.io.*;
 import java.util.*;
+import java.util.regex.Pattern; // concept: import a single symbol from a package
+
+public class AllInOne {
+    // Q: What is static? What do the other keywords do?
+    public static void main (String[] args) throws IOException {
+        String[] vanilla = { "ice", "ice", "baby" };
+        System.out.println("--------------- START ---------------");
+
+        OutputMgr oMan = new OutputMgr();
+
+        MyUtil utilObject = new MyUtil();
+        oMan.omPrint( utilObject.fileIo() );
+        oMan.omPrint( utilObject.doHash() );
+
+        int i = 1;
+        if ( args.length > 0 ) {
+            i = Integer.parseInt(args[0]); // concept: command line argument
+        }
+        oMan.omPrint( utilObject.doSwitch(i) );
+        oMan.omPrint( utilObject.printArgs(args) );
+        oMan.omPrintLines( vanilla );
+
+        BasketBall mrBall = new BasketBall ( 7, "square", 3 ); // concept: object, class instance
+        mrBall.setOrangeness(2);   // concept: call method
+        oMan.omPrint(  mrBall.getOutput() );
+
+        System.out.println("---------------  END  ---------------"); System.exit(0); // concept: exit
+        System.out.println("|-|-|-|-|-|-|-|  DIE  |-|-|-|-|-|-|-|"); System.exit(1); // concept: die
+    }
+}
 
 // concept: class
 class MyUtil {
+    public MyUtil( ) { }
 
-    public MyUtil( ) {
-    }
-
-    public void fileIo     ( ) throws IOException {
+    public String fileIo     ( ) throws IOException {
+        String returnString = "";
         BufferedReader br = new BufferedReader(new FileReader("input.txt"));
         BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"));
         String line;
         while ((line = br.readLine()) != null) {
             bw.write(line);
             bw.newLine();
-            System.out.println(line);
+            returnString += line + "\n";
         }
         br.close();
         bw.close();
+        return returnString;
     }
 
-    public void printArgs ( String[] inArgs ) {
-        String spaces = "            ";
+    public String printArgs ( String[] inArgs ) {
+        String returnString = "";
         for (String s: inArgs) { // concept: for loop, array loop
-            System.out.println( spaces + s );
+            returnString += s;
         }
+        return returnString;
     }
 
-    public void doSwitch ( int inputNumber ) {
-        String outputString;
-        String spaces = "        ";
+    public String doSwitch ( int inputNumber ) {
+        String returnString = "";
 
         switch (inputNumber) { // concept: switch
-            case 1:  outputString = spaces + "Foo";
+            case 1:  returnString = "Foo";
                      break;
-            case 3:  outputString = spaces + "Bar";
+            case 7:  returnString = "Bar";
                      break;
-            default: outputString = spaces + "Invalid";
+            default: returnString = "Invalid";
                      break;
         }
-        System.out.println(outputString); // concept: console output
-
+        return returnString;
     }
-    public void doHash ( ) {
-        String spaces = "    ";
+    public String doHash ( ) {
+        String returnString = "";
         // Create a hash map
         HashMap<String, Double> hm = new HashMap<>();
         // Put elements to the map
@@ -57,96 +86,86 @@ class MyUtil {
         // Display elements
         while(i.hasNext()) {
             Map.Entry me = (Map.Entry)i.next();
-            System.out.print(spaces + me.getKey() + ": ");
-            System.out.println(me.getValue());
+            returnString += me.getKey() + ": " + me.getValue() + "\n";
         }
-        System.out.println();
         // Deposit 1000 into Zara's account
         double balance = ((Double)hm.get("Zara")).doubleValue();
         hm.put("Zara", new Double(balance + 1000));
-        System.out.println(spaces + "Zara's new balance: " +
-        hm.get("Zara"));
-
+        returnString += "Zara's new balance: " + hm.get("Zara") + "\n";
+        return returnString;
     }
 }
 
 class Spacer {
-    public String spaces;
+    public String spaces = "";
     public Spacer ( ) { }
     public void bumpSpace  ( ) { spaces += "    "; }
     public String getSpace ( ) { return spaces;    }
 }
 
 class OutputMgr {
-    public OutputMgr ( ) { }
-    public void omPrint  ( String[] omLines ) {
-        for ( String s: omLines ) {
+    public Spacer omSpacer;
+    public OutputMgr ( ) {
+        omSpacer = new Spacer();
+    }
+    public void omPrint  ( String omLines ) {
+        omLines = omLines.replaceAll("(?m)^", omSpacer.getSpace());
+        System.out.println( omLines ); // concept: console output
+        omSpacer.bumpSpace();
+    }
+    public void omPrintLines ( String[] lines ) {
+        String returnString = "";
+        for (String s: lines) {
             System.out.println( s );
         }
     }
+
 }
 
-class Ball {
+class BallDaddy {
     public int size;
+    public String outputString;
     public String shape;
-    public Ball ( int startSize, String startShape ) {
+    public BallDaddy ( int startSize, String startShape ) {
         size = startSize;
         shape = startShape;
+        outputString = "";
         reportInfo();
     }
     public void reportInfo() {
-        String spaces = "                ";
-        System.out.println(
-            spaces + "Ball:"
-            + " size: " + size
-            + " shape: "   + shape
-        );
+        outputString += "Ball:"
+                     + " size: " + size
+                     + " shape: "   + shape;
     }
-    public void setSize  ( int newValue     ) { size  = newValue ; reportInfo(); }
-    public void setShape ( String newValue  ) { shape = newValue ; reportInfo(); }
+    public String getOutput() {
+        return outputString;
+    }
+    public void clearOutput() {
+        outputString = "";
+    }
+    public void setSize  ( int newValue     ) { size  = newValue; reportInfo(); }
+    public void setShape ( String newValue  ) { shape = newValue; reportInfo(); }
+}
+
+class Ball extends BallDaddy { // subclasses have to add newline I guess?
+    public Ball ( int startSize, String startShape ) {
+        super(startSize, startShape);
+    }
+    public void reportInfo() { super.reportInfo(); outputString += "\n"; }
 }
 
 // concept: subclass
-class BasketBall extends Ball {
+class BasketBall extends BallDaddy {
     public int orangeness;
     public BasketBall ( int startSize, String startShape,
                         int startOrangeness ) {
         super(startSize, startShape);
         orangeness = startOrangeness;
     }
-    public void setOrangeness( int newValue ) { orangeness = newValue; }
-}
-
-public class AllInOne {
-    // Q: What is static? What do the other keywords do?
-    public static void main (String[] args) throws IOException {
-        String[] myLines = new String[3];
-        System.out.println("--------------- START ---------------");
-
-        Spacer mySpacer = new Spacer();
-
-        OutputMgr oMan = new OutputMgr();
-        myLines[0] = "ice";
-        myLines[1] = "ice";
-        myLines[2] = "baby";
-        oMan.omPrint(myLines);
-
-        MyUtil utilObject = new MyUtil();
-        utilObject.fileIo();
-        utilObject.doHash();
-
-        int i = 1;
-        if ( args.length > 0 ) {
-            i = Integer.parseInt(args[0]); // concept: command line argument
-        }
-        utilObject.doSwitch(i);
-        utilObject.printArgs(args);
-
-        BasketBall mrBall = new BasketBall ( 7, "square", 3 ); // concept: object, class instance
-        mrBall.setSize(2);                     // concept: call method
-
-        System.out.println("---------------  END  ---------------"); System.exit(0); // concept: exit
-        System.out.println("|-|-|-|-|-|-|-|  DIE  |-|-|-|-|-|-|-|"); System.exit(1); // concept: die
+    public void reportInfo() {
+        super.reportInfo();
+        outputString += " orangeness: "   + orangeness + "\n";
     }
+    public void setOrangeness( int newValue  ) { orangeness = newValue; reportInfo(); }
 }
 
